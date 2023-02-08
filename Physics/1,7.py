eaf4e098e4414f3cdb879de0b6aebaeeebddada4
Olle-Langwagen <olle.langwagen@elev.ga.ntig.se>
2023-02-08 13:55:22 +0100

    Update 1,7.py

--- Physics/1,7.py
+++ Physics/1,7.py
@@ -1,9 +1,22 @@
 from cmu_graphics import *
 
 fire=Rect(100,100,100,100)
+material=Circle(300,600,40,fill='red')
 
-def OnMouseDrag(MouseX, MouseY):
-    fire.centerX=MouseX
-    fire.centerY=MouseY
+def onMouseDrag(MouseX, MouseY):
+    material.centerX=MouseX
+    material.centerY=MouseY
+    if fire.contains(material.centerX, material.centerY):
+        pass
 
+#Increase the temperature of the material if the fire contains material
+def increaseTemperature():
+    if fire.contains(material.centerX, material.centerY):
+        material.fill='orange'
+    else:
+        material.fill='red'
+running = True
+while running:
+    increaseTemperature()
+    
 cmu_graphics.run()
