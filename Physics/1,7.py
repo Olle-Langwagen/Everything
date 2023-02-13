3454330c1213f43de9ad603f5bc7219dcd4491c9
Olle-Langwagen <olle.langwagen@elev.ga.ntig.se>
2023-02-13 20:35:08 +0100

    Update 1,7.py

--- Physics/1,7.py
+++ Physics/1,7.py
@@ -1,34 +1,60 @@
 from cmu_graphics import *
 
+
+#Temperature increase and decrease formula: T = T + 1 if the mouse is inside the fire, T = T - 1 if the mouse is inside the frigde, T = T + 0 if the mouse is outside the fire and the frigde
+app.stepsPerSecond = 10
+
+#Material values
+mass = 1
+c = 0.90
+
 #Create a fire
 fire=Rect(100,100,100,100, fill="red")
-material=Circle(300,600,40,fill=(gradient('red','orange')))
+
+#Create a frigde
+frigde=Rect(500,100,100,100,fill="blue")
+
 #Create a thermometer
-thermometerScale=Rect(700,100,50,400,fill=(gradient('blue','red')))
+thermometerScale=Rect(700,100,50,400,fill=(gradient('blue','red', start='bottom')))
+
+#Create a temperature variable
+temperature = Label(0, 20, 20, size=50)
 
 #Create a thermometer pointer
-thermometerPointer=Rect(700,500,50,10,fill='black')
+thermometerPointer=Rect(700,300,50,10,fill='black')
 
-#The temperature of the material
-temperature=0
+#Create the material
+material=Circle(300,600,40,fill=(gradient('red','orange')))
+
 
 #Move the material with the mouse, if the mouse is inside the fire, the temperature of the material increases, if the mouse is outside the fire, the temperature of the material decreases
 def onMouseDrag(MouseX, MouseY):
+    global temperature
     material.centerX=MouseX
     material.centerY=MouseY
     
-    print(temperature)
-
-#Move the thermometer pointer according to the temperature of the material
-def onFrame():
-    thermometerPointer.centerY=500-temperature*4
-
-def onstep():
+def onStep():
     if fire.contains(material.centerX,material.centerY):
-        pass
-    temperature+=1
-    thermometerPointer.centerY=500-temperature*4
-    print(temperature)
+        temperature.value += 1
+        thermometerPointer.centerY = 300 - temperature.value * 4
+        print(temperature.value)
+    elif frigde.contains(material.centerX,material.centerY):
+        temperature.value -= 1
+        thermometerPointer.centerY = 300 - temperature.value * 4
+        print(temperature.value)
+    #Increase the thermometer pointer according to the temperature of the material, but the thermometer pointer can't go above the top of the thermometer scale and below the bottom of the thermometer scale
+    if thermometerPointer.centerY > 500:
+        thermometerPointer.centerY = 500
+    elif thermometerPointer.centerY < 100:
+        thermometerPointer.centerY = 100
+        
+    
+
+    
+    
+    #Change the color of the material according to the temperature of the material
+    #material.fill=(gradient('red','orange',start=temperature.value/100))
+
     
 
 #Run the program
